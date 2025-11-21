471a47006a9723b43be0c1f1d4878a5e837bafe1
florestomas <floresmancillatomas@gmail.com>
2025-11-21 15:43:16 -0300

    settings.json

---
+++ zoneless-calculator-fr/src/app/calculator/components/calculator-button/calculator-button.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-button',
+  imports: [],
+  templateUrl: './calculator-button.html',
+})
+export class CalculatorButton { }
