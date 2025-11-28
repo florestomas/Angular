c20443c1e5aec5775660c58411be08de6a252fb9
florestomas <floresmancillatomas@gmail.com>
2025-11-28 11:14:57 -0300

    Mi primera prueba

--- zoneless-calculator-fr/src/app/app.spec.ts
+++ zoneless-calculator-fr/src/app/app.spec.ts
@@ -12,15 +12,35 @@ describe('App', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
+    const app = fixture.componentInstance; //Es la instancia de nuestro appComponent
+
     expect(app).toBeTruthy();
   });
 
+  it('should be 3', () => {
+    // AAA, Arrange, Act, Assert
+    // Arrange
+    const num1 = -2;
+    const num2 = 5;
+
+    // Act
+    const result = num1 + num2;
+
+    // Assert
+    /* if (result !== 3) { OBSOLETO, HAY Q USAR expect()
+      throw new Error('no es 3');
+    } */
+
+    expect(result).toBe(3);
+  });
+
   //it('should render title', () => {
-  // const fixture = TestBed.createComponent(App);
-  //fixture.detectChanges();
-  // const compiled = fixture.nativeElement as HTMLElement;
-  // expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator-fr');
+  //  const fixture = TestBed.createComponent(App);
+  //  fixture.detectChanges();
+  //
+  //  const compiled = fixture.nativeElement as HTMLElement;
+  //  console.log(compiled); // nuestro codigo HTL del app component
+  //
+  //  //expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator-fr');
   //});
-  // add more tests here
 });
